85fec5e27422a9de9ac17f8bf0b7c1e3031a4fdf
Jason Torres Luis <jtorresl79c@gmail.com>
2023-07-12 15:42:45 -0700

    94. Getting Started with the React Navigation Package

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import {
   SafeAreaView
 } from 'react-native-safe-area-context';
@@ -9,26 +12,31 @@ import {
 
 import CategoriesScreen from './screens/CategoriesScreen';
 
+const Stack = createNativeStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <>
+      <StatusBar style="dark" />
 
-      <SafeAreaView style={styles.rootScreen}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <CategoriesScreen />
-      </SafeAreaView>
 
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+
+
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
