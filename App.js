21633fca0931fe795ab97f48fc9e0bdbb66490ec
Jason Torres Luis <jtorresl79c@gmail.com>
2023-07-18 07:28:10 -0700

    112. Bottom Tabs & App Finishing Touches

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {Ionicons} from '@expo/vector-icons'
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -25,12 +27,19 @@ function DrawerNavigator() {
     <Drawer.Navigator screenOptions={{
       headerStyle: { backgroundColor: '#351401' },
       headerTintColor: 'white',
-      sceneContainerStyle: { backgroundColor: '#3f2f25' }
+      sceneContainerStyle: { backgroundColor: '#3f2f25' },
+      drawerContentStyle: {backgroundColor: '#351401'},
+      drawerInactiveTintColor: 'white',
+      drawerActiveTintColor: '#351401',
+      drawerActiveBackgroundColor: '#e4baa1'
     }}>
       <Drawer.Screen name="Categories" component={CategoriesScreen} options={{
-        title: 'All categories'
+        title: 'All categories',
+        drawerIcon: ({color, size}) => <Ionicons name="list" color={color} size={size} />
       }}/>
-      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} options={{
+        drawerIcon: ({color, size}) => <Ionicons name="star" color={color} size={size} />
+      }}/>
     </Drawer.Navigator>
   )
 }
